4b0eceed450aca0c96ed8af91b509482265ba252
hzdiella06 <haziridiella@gmail.com>
2026-06-18 13:30:08 +0200

    feat: regjister me jwt token
    kom shtu logjiken per krijimin e userit dhe kthimin e jwt token

--- backend/Services/AuthService.cs
+++ backend/Services/AuthService.cs
@@ -2,19 +2,77 @@ using backend.DTOs;
 using backend.Interfaces;
 using backend.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace backend.Services;
 
-public class AuthService : IAuthService{
+public class AuthService : IAuthService
+{
     private UserManager<ApplicationUser> userManager;
+    private IConfiguration configuration;
 
-    public AuthService(UserManager<ApplicationUser> userManager){
+    public AuthService(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+    {
         this.userManager = userManager;
+        this.configuration = configuration;
     }
-    public Task<AuthResponseDto?> RegisterAsync(RegisterDto registerDto){
-        return Task.FromResult<AuthResponseDto?>(null);
+
+    private string GenerateToken(ApplicationUser user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim(ClaimTypes.Email, user.Email ?? ""),
+            new Claim("fullName", user.FullName)
+        };
+
+        var key = configuration["Jwt:Key"];
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["Jwt:Issuer"],
+            audience: configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(60),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
-    public Task<AuthResponseDto?> LoginAsync(LoginDto loginDto){
+
+   public async Task<AuthResponseDto?> RegisterAsync(RegisterDto registerDto)
+{
+    var user = new ApplicationUser
+    {
+        FullName = registerDto.FullName,
+        Email = registerDto.Email,
+        UserName = registerDto.Email
+    };
+
+    var result = await userManager.CreateAsync(user, registerDto.Password);
+
+    if (!result.Succeeded)
+    {
+        return null;
+    }
+
+    var token = GenerateToken(user);
+
+    return new AuthResponseDto
+    {
+        Token = token,
+        Email = user.Email ?? "",
+        FullName = user.FullName
+    };
+}
+
+    public Task<AuthResponseDto?> LoginAsync(LoginDto loginDto)
+    {
         return Task.FromResult<AuthResponseDto?>(null);
     }
 }
